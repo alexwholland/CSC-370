81e7ace041ca5aa9e534df11a9554263f3569861
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-06-30 20:03:33 -0700

    Testing out setup

---
+++ Assignment 3/query10.sql
@@ -0,0 +1,11 @@
+-- Retrieve the largest labour force in a
+-- single county
+-- 1.1 marks: <2 operators
+-- 1.0 marks: <3 operators
+-- 0.9 marks: <5 operators
+-- 0.8 marks: correct answer
+
+-- Replace this comment line with the actual query
+FROM `countylabourstats`
+SELECT `labour_force` = (SELECT MAX(`labour_force`) FROM `countylabourstats`);
+FROM
