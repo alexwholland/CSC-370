e71dee0ba1e1c867691ee6ba580f484d6dcd3516
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-02 16:04:15 -0700

    Query 5 complete with bonus

--- Assignment 3/query05.sql
+++ Assignment 3/query05.sql
@@ -7,4 +7,32 @@
 -- 1.0 marks: <15 operators
 -- 0.8 marks: correct answer
 
--- Replace this comment line with the actual query
+-- This method uses implicit Join syntax (not recommended)
+-- 7 Golf Operators
+SELECT 
+    c.name, 
+    s.abbr,
+    cls.labour_force,
+    (cls.unemployed / cls.labour_force) * 100 
+        AS "Unemployment Rate"
+FROM county c, countylabourstats cls, state s
+WHERE c.fips = cls.county
+    AND c.state = s.id
+    AND (cls.unemployed / cls.labour_force) > 0.1
+    AND cls.year = 2008
+ORDER BY cls.labour_force DESC;
+
+-- Original solution (recommended)
+-- 9 Golf Operators
+-- SELECT 
+--     c.name, 
+--     s.abbr,
+--     cls.labour_force,
+--     (cls.unemployed / cls.labour_force) * 100 
+--         AS "Unemployment Rate"
+-- FROM county c
+-- JOIN countylabourstats cls ON cls.county = c.fips
+-- JOIN state s ON s.id = c.state
+-- WHERE (cls.unemployed / cls.labour_force) > 0.1 
+--     AND cls.year = 2008
+-- ORDER BY cls.labour_force DESC;
