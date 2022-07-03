77fc10215d7e171ad30c5ea66a40cc684c0e26a6
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-02 19:54:38 -0700

    Query working
    - Query fully works
    - Is in operator range for bonus marks
    - Still requires some testing

--- Assignment 3/query11.sql
+++ Assignment 3/query11.sql
@@ -8,4 +8,29 @@
 -- 0.8 marks: correct answer
 
 
--- Replace this comment line with the actual query
+-- This method uses implicit Join syntax (not recommended)
+-- 6 Golf Operators
+SELECT DISTINCT 
+    s.abbr
+FROM county c, countylabourstats cls, state s
+WHERE c.fips = cls.county
+    AND c.state = s.id
+    AND (cls.unemployed / cls.labour_force) > 0.06
+    AND cls.year = 2008
+GROUP BY c.state
+HAVING COUNT(c.state) > 100
+ORDER BY s.abbr;
+
+
+-- Original solution (recommended)
+-- 8 Golf Operators
+-- SELECT DISTINCT 
+--     s.abbr
+-- FROM county c
+-- JOIN countylabourstats cls ON cls.county = c.fips
+-- JOIN state s ON s.id = c.state
+-- WHERE (cls.unemployed / cls.labour_force) > 0.06
+--     AND cls.year = 2008
+-- GROUP BY c.state
+-- HAVING COUNT(c.state) > 100
+-- ORDER BY s.abbr;
