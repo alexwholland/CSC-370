ce48fcc8efa87f08a87e25b43963603285282e29
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-08 09:25:23 -0700

    Complete
    Required further simplification to reduce the golf operators

--- Assignment 3/query07.sql
+++ Assignment 3/query07.sql
@@ -6,4 +6,32 @@
 -- 0.8 marks: correct answer
 
 
--- Replace this comment line with the actual query
+-- 22 Golf Operators 
+
+SELECT 
+    N.name, 
+    N.2010, 
+    N.population AS '2019', 
+    N.abbr, 
+    ABS(MIN(N.Percentage)) AS 'Loss (%)'
+FROM(
+    SELECT 
+    *, 
+    (N.population - LAG(N.population, 1) 
+    OVER (PARTITION BY N.fips)) / LAG(N.population, 1) 
+    OVER (PARTITION BY N.fips) * 100 AS 'Percentage', 
+    LAG(N.population, 1) 
+    OVER (PARTITION BY N.fips) AS '2010'
+    FROM(
+        SELECT * 
+        FROM county c 
+        JOIN state s ON s.id = c.state 
+        JOIN countypopulation p ON p.county = c.fips
+        WHERE p.year = 2010 OR p.year = 2019
+    ) AS N
+    ORDER BY Percentage ASC
+) AS N
+WHERE N.Percentage = (SELECT(MIN(N.Percentage)))
+LIMIT 1 OFFSET 1;
+
+WHERE N.Percentage = (SELECT(MIN(N.Percentage)))
