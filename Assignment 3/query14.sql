762bbf9ba69cf48e4b8c2aae4b3b7708b81d1c90
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-23 11:49:22 -0700

    Query 14 complete and refactored

--- Assignment 3/query14.sql
+++ Assignment 3/query14.sql
@@ -1,9 +1,28 @@
 -- Out of those counties with at least 25000 residents,
--- retrieve the pair that had the absolute closest
+-- retrieve the pair from the same state
+-- that had the absolute closest
 -- population in 2018
 -- 1.1 marks: <11 operators
 -- 1.0 marks: <12 operators
 -- 0.9 marks: <14 operators
 -- 0.8 marks: correct answer
 
--- Replace this comment line with the actual query
+-- 12 Golf Operators 
+WITH Q AS (
+    SELECT * 
+    FROM 
+        countypopulation cp, 
+        county c
+    WHERE cp.county = c.fips
+    AND cp.year = 2018
+    AND cp.population >= 25000 
+)
+SELECT 
+    b.name, 
+    b.population, 
+    a.name, 
+    b.population 
+FROM Q a
+JOIN Q b ON a.county <> b.county AND a.state = b.state
+ORDER BY ABS(a.population - b.population)
+LIMIT 1;
