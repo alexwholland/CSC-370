e319e78bf519d13786646bb393f669d953fe997a
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-23 11:45:09 -0700

    Removed id indentifier

--- Assignment 3/query12.sql
+++ Assignment 3/query12.sql
@@ -1,17 +1,24 @@
 -- Retrieve alphabetically the names of industries that
--- employ at least five million workers across
+-- employ at least ten million workers across
 -- the US, excluding California.
 -- 1.1 marks: <9 operators
 -- 1.0 marks: <11 operators
 -- 0.9 marks: <14 operators
 -- 0.8 marks: correct answer
 
--- 7 Golf Operators
+-- 12 Golf Operators
 SELECT i.name
-FROM industry i
-WHERE (
-    SELECT SUM(s.employees) FROM countyindustries s
-    WHERE s.industry = i.id
-    AND s.county NOT LIKE '6%'
-) >=5000000
-ORDER BY i.name ASC;
+FROM 
+    countyindustries ci, 
+    industry i
+WHERE 
+    ci.industry = i.id 
+    AND ci.county NOT IN (
+	    SELECT c.fips
+	    FROM county c, state s
+        WHERE s.id = c.state
+        AND s.abbr = 'CA'
+    )
+GROUP BY ci.industry
+HAVING SUM(ci.employees) >= 5000000
+ORDER BY i.name;
