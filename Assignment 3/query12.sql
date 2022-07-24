d5369fba16246b3337d5a9fc1b4c99a416b32c4e
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-07-23 18:39:58 -0700

    Updated specifications and code

--- Assignment 3/query12.sql
+++ Assignment 3/query12.sql
@@ -1,24 +1,23 @@
 -- Retrieve alphabetically the names of industries that
--- employ at least ten million workers across
+-- employ at least five million workers across
 -- the US, excluding California.
 -- 1.1 marks: <9 operators
 -- 1.0 marks: <11 operators
 -- 0.9 marks: <14 operators
 -- 0.8 marks: correct answer
 
--- 12 Golf Operators
-SELECT i.name
-FROM 
-    countyindustries ci, 
-    industry i
-WHERE 
-    ci.industry = i.id 
-    AND ci.county NOT IN (
-	    SELECT c.fips
-	    FROM county c, state s
-        WHERE s.id = c.state
-        AND s.abbr = 'CA'
-    )
-GROUP BY ci.industry
-HAVING SUM(ci.employees) >= 5000000
+-- 9 Golf Operators
+SELECT i.name 
+FROM industry i
+WHERE (
+    SELECT SUM(ci.employees) 
+    FROM 
+        countyindustries ci, 
+        county c, 
+        state s
+    WHERE ci.county = c.fips
+    AND c.state = s.id 
+    AND ci.industry = i.id 
+    AND s.abbr != 'CA'
+) >= 5000000
 ORDER BY i.name;
